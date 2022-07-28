542968b55a1baae2b1e0a38be098758608cd9140
archan bhatta <archanme1@gmail.com>
2022-07-27 23:11:36 -0400

    portfolio site complete

---
+++ app.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+const hLink = document.querySelectorAll("#hLink");
+const body = document.querySelector("body");
+const moon = document.querySelector("#moon");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  hamburger.classList.toggle("bg-white");
+});
+
+hLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    hamburger.classList.toggle("bg-white");
+  });
+});
+
+moon.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
